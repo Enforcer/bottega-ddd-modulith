ca139a3ae4b447d933478de97ad31e903956f9f8
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-18 20:39:26 +0200

    Add likes API to makeup for lost code

--- used_stuff_market/api/likes.py
+++ used_stuff_market/api/likes.py
@@ -1,3 +1,16 @@
+from uuid import UUID
+
+from fastapi import Header
 from fastapi.routing import APIRouter
 
 router = APIRouter()
+
+
+@router.post("/items/{item_id}/like")
+def like(item_id: int, user_id: UUID = Header()):
+    pass
+
+
+@router.delete("/items/{item_id}/like")
+def unlike(item_id: int, user_id: UUID = Header()):
+    pass
