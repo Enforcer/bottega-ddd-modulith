3709e82128b1ac4e931b5730f19dfacac99b5c8d
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-23 21:03:24 +0200

    Introduce Port in items component by extracting repo to items_infrastructure

--- used_stuff_market/api/items.py
+++ used_stuff_market/api/items.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 
 from used_stuff_market.api.session_deps import get_session
 from used_stuff_market.items import Items
+from used_stuff_market.items_infrastructure.repository import SqlAlchemyItemsRepository
 from used_stuff_market.shared_kernel.money import Currency, Money
 
 router = APIRouter()
@@ -32,13 +33,17 @@ class AddItemData(BaseModel):
 def add(
     data: AddItemData, user_id: UUID = Header(), session: Session = Depends(get_session)
 ) -> Response:
-    items = Items()
+    repository = SqlAlchemyItemsRepository(session=session)
+    items = Items(repository=repository)
     items.add(**data.dict(), owner_id=user_id)
     session.commit()
     return Response(status_code=204)
 
 
 @router.get("/items")
-def get_items(user_id: UUID = Header()) -> list[dict]:
-    items = Items()
+def get_items(
+    user_id: UUID = Header(), session: Session = Depends(get_session)
+) -> list[dict]:
+    repository = SqlAlchemyItemsRepository(session=session)
+    items = Items(repository=repository)
     return items.get_items(owner_id=user_id)  # type: ignore
--- used_stuff_market/items/repository.py
+++ used_stuff_market/items/repository.py
@@ -1,49 +1,15 @@
-from sqlalchemy import Column, Integer, Numeric, String, Table
+import abc
+
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import composite
 
-from used_stuff_market.db import ScopedSession, mapper_registry, metadata
 from used_stuff_market.items.item import Item
-from used_stuff_market.shared_kernel.money import Currency, Money
 
 
-class ItemsRepository:
+class ItemsRepository(abc.ABC):
+    @abc.abstractmethod
     def add(self, item: Item) -> None:
-        session = ScopedSession()
-        session.add(item)
-        session.flush()
+        pass
 
+    @abc.abstractmethod
     def for_owner(self, owner_id: UUID) -> list[Item]:
-        session = ScopedSession()
-        items: list[Item] = (
-            session.query(Item).filter(Item.owner_id == str(owner_id)).all()
-        )
-        return items
-
-
-items = Table(
-    "items",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("owner_id", UUID(as_uuid=True)),
-    Column("title", String()),
-    Column("description", String()),
-    Column("starting_price_amount", Numeric()),
-    Column("starting_price_currency", String(3)),
-    schema="items",
-)
-
-
-mapper_registry.map_imperatively(
-    Item,
-    items,
-    properties={
-        "starting_price": composite(
-            lambda currency_code, amount: Money(
-                Currency.from_code(currency_code), amount
-            ),
-            items.c.starting_price_currency,
-            items.c.starting_price_amount,
-        ),
-    },
-)
+        pass
