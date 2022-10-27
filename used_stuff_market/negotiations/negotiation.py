155973bdc89bfbab29877dff3629f9e68c24a037
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-27 16:31:33 +0200

    Strategy pattern

--- used_stuff_market/negotiations/negotiation.py
+++ used_stuff_market/negotiations/negotiation.py
@@ -1,5 +1,9 @@
 from uuid import UUID
 
+from used_stuff_market.negotiations.strategies import (
+    NegotiationStrategy,
+    NoRestrictions,
+)
 from used_stuff_market.shared_kernel.money import Money
 
 
@@ -20,7 +24,13 @@ class CannotCounterOwnOffer(Exception):
 
 
 class Negotiation:
-    def __init__(self, offer: Money, offerer: UUID, offeree: UUID) -> None:
+    def __init__(
+        self,
+        offer: Money,
+        offerer: UUID,
+        offeree: UUID,
+        price_verification_strategy: NegotiationStrategy = NoRestrictions(),
+    ) -> None:
         """
         :param offer: how much was originally offered?
         :param oferrer: who offered that price?
@@ -31,6 +41,7 @@ class Negotiation:
         self._offeree = offeree
         self._ended = False
         self._who_offers = offerer
+        self._price_verification = price_verification_strategy
 
     @property
     def offer(self) -> Money:
@@ -51,6 +62,9 @@ class Negotiation:
         self._ended = True
 
     def propose_counter_offer(self, party: UUID, counter_offer: Money) -> None:
+        self._price_verification.validate_offer(
+            current_offer=self._offer, new_offer=counter_offer
+        )
         if self._ended:
             raise NegotiationClosed
         if self._who_offers == party:
--- used_stuff_market/negotiations/strategies.py
+++ used_stuff_market/negotiations/strategies.py
@@ -2,7 +2,7 @@ import abc
 import random
 from uuid import UUID
 
-from used_stuff_market.shared_kernel.money import Money
+from used_stuff_market.shared_kernel.money import Currency, Money
 
 
 class NegotiationStrategy(abc.ABC):
@@ -12,6 +12,25 @@ class NegotiationStrategy(abc.ABC):
     @abc.abstractmethod
     def validate_offer(self, current_offer: Money, new_offer: Money) -> None:
         pass
+
+
+class NoRestrictions(NegotiationStrategy):
+    def validate_offer(self, current_offer: Money, new_offer: Money) -> None:
+        pass
+
+
+class FivePercentLimit(NegotiationStrategy):
+    def validate_offer(self, current_offer: Money, new_offer: Money) -> None:
+        difference = abs(new_offer.amount - current_offer.amount) / current_offer.amount
+        if difference > 0.05:
+            raise self.PriceNotAllowed
+
+
+class FiveDollarsLimit(NegotiationStrategy):
+    def validate_offer(self, current_offer: Money, new_offer: Money) -> None:
+        max_difference = Money(Currency.from_code("USD"), "5.00")
+        if abs(new_offer.amount - current_offer.amount) > max_difference.amount:
+            raise self.PriceNotAllowed
 
 
 def get_strategy_for_party(party: UUID) -> NegotiationStrategy:
