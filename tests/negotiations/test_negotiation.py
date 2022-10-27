62b6a5b5eaa3138b13c9ca58c2c574c2c7daa4c6
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-27 08:27:01 +0200

    Implement basic negotiation

--- tests/negotiations/test_negotiation.py
+++ tests/negotiations/test_negotiation.py
@@ -1,15 +1,58 @@
-from uuid import uuid4
+from uuid import UUID, uuid4
 
 import pytest
 
-from used_stuff_market.negotiations.negotiation import Negotiation
+from used_stuff_market.negotiations.negotiation import (
+    CannotAcceptOwnOffer,
+    CannotRejectOwnOffer,
+    Negotiation,
+    NegotiationClosed,
+)
 from used_stuff_market.shared_kernel.money import Currency, Money
 
 
-@pytest.mark.skip()
-def test_negotiation() -> None:
-    negotiation = Negotiation(  # noqa
-        offer=Money(Currency.from_code("USD"), "9.99"), offerer=uuid4(), offeree=uuid4()
+@pytest.fixture()
+def offerer() -> UUID:
+    return uuid4()
+
+
+@pytest.fixture()
+def offeree() -> UUID:
+    return uuid4()
+
+
+@pytest.fixture()
+def negotiation(offerer: UUID, offeree: UUID) -> Negotiation:
+    return Negotiation(  # noqa
+        offer=Money(Currency.from_code("USD"), "9.99"), offerer=offerer, offeree=offeree
     )
 
-    ...
+
+def test_cannot_reject_accepted_negotiation(
+    negotiation: Negotiation, offeree: UUID
+) -> None:
+    negotiation.accept_offer(party=offeree)
+    with pytest.raises(NegotiationClosed):
+        negotiation.reject_offer(party=offeree)
+
+
+def test_cannot_accept_accepted_negotiation(
+    negotiation: Negotiation, offeree: UUID
+) -> None:
+    negotiation.accept_offer(party=offeree)
+    with pytest.raises(NegotiationClosed):
+        negotiation.accept_offer(party=offeree)
+
+
+def test_cannot_accept_offer_as_offerer(
+    negotiation: Negotiation, offerer: UUID
+) -> None:
+    with pytest.raises(CannotAcceptOwnOffer):
+        negotiation.accept_offer(party=offerer)
+
+
+def test_cannot_reject_offer_as_offerer(
+    negotiation: Negotiation, offerer: UUID
+) -> None:
+    with pytest.raises(CannotRejectOwnOffer):
+        negotiation.reject_offer(party=offerer)
