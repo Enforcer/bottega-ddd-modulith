8d691f608b37c7458a6edc91e5ee671cd7688276
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-29 21:31:48 +0200

    Use injected session in Items repository

--- used_stuff_market/main.py
+++ used_stuff_market/main.py
@@ -1,8 +1,11 @@
 from lagom import Container
+from sqlalchemy.orm import Session
 
+from used_stuff_market.db import ScopedSession
 from used_stuff_market.items import ItemsRepository
 from used_stuff_market.items_infrastructure.repository import SqlAlchemyItemsRepository
 
 container = Container()
 
 container[ItemsRepository] = SqlAlchemyItemsRepository  # type: ignore
+container[Session] = lambda _: ScopedSession()
