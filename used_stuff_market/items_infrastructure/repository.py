c0f23cca47a153a9683c71a668e34fe3d53ef121
Sebastian Buczyński <nnplaya@gmail.com>
2022-10-29 21:31:48 +0200

    Use injected session in Items repository

--- used_stuff_market/items_infrastructure/repository.py
+++ used_stuff_market/items_infrastructure/repository.py
@@ -1,7 +1,6 @@
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Session
 
-from used_stuff_market.db import ScopedSession
 from used_stuff_market.items.item import Item
 from used_stuff_market.items.repository import ItemsRepository
 
@@ -11,13 +10,11 @@ class SqlAlchemyItemsRepository(ItemsRepository):
         self._session = session
 
     def add(self, item: Item) -> None:
-        session = ScopedSession()
-        session.add(item)
-        session.flush()
+        self._session.add(item)
+        self._session.flush()
 
     def for_owner(self, owner_id: UUID) -> list[Item]:
-        session = ScopedSession()
         items: list[Item] = (
-            session.query(Item).filter(Item.owner_id == str(owner_id)).all()
+            self._session.query(Item).filter(Item.owner_id == str(owner_id)).all()
         )
         return items
