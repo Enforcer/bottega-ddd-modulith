1923112e919f505d1b1310780bb08ff4711b7c20
Sebastian Buczyński <nnplaya@gmail.com>
2022-11-09 08:56:00 +0100

    Use SnowflakeGateway stub in checker tests

--- tests/auditor/test_due_payments.py
+++ tests/auditor/test_due_payments.py
@@ -8,49 +8,54 @@ from used_stuff_market.auditor import due_payments
 from used_stuff_market.auditor.snowflake_gateway import SnowflakeGateway
 
 
+class SnowflakeGatewayStub(SnowflakeGateway):
+    """Stubs can be implemented manually, e.g. by subclassing."""
+
+    def __init__(self, rows_to_return: list[tuple]) -> None:
+        self._rows_to_return = rows_to_return
+
+    def fetch_overdue_payments(self) -> list[tuple]:
+        return self._rows_to_return
+
+
 @freeze_time("2015-01-01")
 def test_due_payments() -> None:
-    with mock.patch("snowflake.connector.connect") as mock_snowflake_conn:
-        cursor_mock = mock.MagicMock()
-        cursor_mock.return_value.__enter__.return_value.fetchall.return_value = [
-            (
-                "90d9a448-9555-41bf-a983-76bf5ffb74e3",
-                datetime(2020, 1, 1, 12, 10, 30),
-                1,
-                datetime(2020, 1, 1, 12, 11, 30),
-                20,
-                "USD",
-            ),
-            (
-                "90d9a448-9555-41bf-a983-76bf5ffb74e3",
-                datetime(2020, 1, 1, 12, 10, 30),
-                1,
-                datetime(2020, 1, 1, 12, 11, 30),
-                10,
-                "USD",
-            ),
-            (
-                "90d9a448-9555-41bf-a983-76bf5ffb74e3",
-                datetime(2020, 1, 1, 12, 10, 30),
-                2,
-                datetime(2020, 1, 1, 12, 11, 30),
-                15,
-                "USD",
-            ),
-        ]
-        mock_snowflake_conn.return_value.cursor.side_effect = cursor_mock
+    snowflake_gateway_stub = mock.Mock(
+        spec_set=SnowflakeGateway,
+        fetch_overdue_payments=mock.Mock(
+            return_value=[
+                (
+                    "90d9a448-9555-41bf-a983-76bf5ffb74e3",
+                    datetime(2020, 1, 1, 12, 10, 30),
+                    1,
+                    datetime(2020, 1, 1, 12, 11, 30),
+                    20,
+                    "USD",
+                ),
+                (
+                    "90d9a448-9555-41bf-a983-76bf5ffb74e3",
+                    datetime(2020, 1, 1, 12, 10, 30),
+                    1,
+                    datetime(2020, 1, 1, 12, 11, 30),
+                    10,
+                    "USD",
+                ),
+                (
+                    "90d9a448-9555-41bf-a983-76bf5ffb74e3",
+                    datetime(2020, 1, 1, 12, 10, 30),
+                    2,
+                    datetime(2020, 1, 1, 12, 11, 30),
+                    15,
+                    "USD",
+                ),
+            ],
+        ),
+    )
 
-        checker = due_payments.DuePaymentsChecker(
-            snowflake_gateway=SnowflakeGateway(
-                username="testuser",
-                password="password",
-                account="testaccount",
-                region="eu-central-1",
-                database="testdatabase",
-                warehouse="default",
-            ),
-            prometheus_host_port="prometheus:9090",
-        )
+    checker = due_payments.DuePaymentsChecker(
+        snowflake_gateway=snowflake_gateway_stub,
+        prometheus_host_port="prometheus:9090",
+    )
     with mock.patch.object(due_payments, "push_to_gateway") as mock_push_to_gateway:
         checker.check()
 
