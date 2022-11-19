c65ec5c26d7817057ed3252eae77f4d1c3429899
Sebastian Buczyński <nnplaya@gmail.com>
2022-11-19 16:28:48 +0100

    Move Facade to separate file to get rid of circular imports

--- used_stuff_market/catalog/tasks.py
+++ used_stuff_market/catalog/tasks.py
@@ -1,11 +1,10 @@
 from used_stuff_market.db import db_session
 from used_stuff_market.workers.with_celery import app
+from used_stuff_market.catalog.facade import Catalog
 
 
 @app.task
 def increase_likes(item_id: int) -> None:
-    from used_stuff_market.catalog import Catalog
-
     with db_session() as session:
         Catalog().increase_likes(item_id=item_id)
         session.commit()
@@ -13,8 +12,6 @@ def increase_likes(item_id: int) -> None:
 
 @app.task
 def decrease_likes(item_id: int) -> None:
-    from used_stuff_market.catalog import Catalog
-
     with db_session() as session:
         Catalog().decrease_likes(item_id=item_id)
         session.commit()
