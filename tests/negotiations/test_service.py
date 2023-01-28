64642fd666f4a2a5e34a9c11af8fe69f789fcab8
Sebastian Buczyński <nnplaya@gmail.com>
2023-01-28 22:36:08 +0100

    Mocks spec_set and sealed

--- tests/negotiations/test_service.py
+++ tests/negotiations/test_service.py
@@ -1,20 +1,24 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, seal
 from uuid import uuid4
 
 import pytest
 
+from used_stuff_market.availability import Availability
+from used_stuff_market.negotiations.negotiation import Negotiation
 from used_stuff_market.negotiations.negotiation_id import NegotiationId
+from used_stuff_market.negotiations.repository import NegotiationsRepository
 from used_stuff_market.negotiations.service import Negotiations
+from used_stuff_market.shared_kernel.money import USD, Money
 
 
 @pytest.fixture()
 def repository() -> Mock:
-    return Mock()
+    return Mock(spec_set=NegotiationsRepository)
 
 
 @pytest.fixture()
 def availability() -> Mock:
-    return Mock()
+    return Mock(spec_set=Availability)
 
 
 @pytest.fixture()
@@ -29,11 +33,20 @@ def test_accepting_negotiation_locks_the_item(
     service: Negotiations, repository: Mock, availability: Mock
 ) -> None:
     negotiation_id = NegotiationId(item_id=-2, offerer=uuid4(), offeree=uuid4())
-    repository.get.return_value = Mock(id=negotiation_id)
+    repository.get.return_value = Negotiation(
+        item_id=2,
+        owner=negotiation_id.offeree,
+        offer=Money(USD, "9.99"),
+        offerer=negotiation_id.offerer,
+        offeree=negotiation_id.offeree,
+    )
+    seal(repository)
+    availability.lock = Mock(return_value=None)
+    seal(availability)
 
     service.accept(negotiation_id=negotiation_id, party=negotiation_id.offeree)
 
-    availability.lock_resource.assert_called_once_with(
+    availability.lock.assert_called_once_with(
         resource_id=negotiation_id.item_id,
         lock_for=negotiation_id.offerer,
     )
