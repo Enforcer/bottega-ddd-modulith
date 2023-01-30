843ff4594dbd2ec4c1e8b828f8b32a7e0ed69d2c
Sebastian Buczyński <nnplaya@gmail.com>
2023-01-30 21:45:18 +0100

    Solution for parametrized tests

--- tests/credits/test_calculator.py
+++ tests/credits/test_calculator.py
@@ -1,15 +1,38 @@
+import pytest
+
 from used_stuff_market.credits.calculator import calculate_lending_capacity
 from used_stuff_market.credits.employment_type import EmploymentType
 from used_stuff_market.shared_kernel.money import USD, Money
 
 
-def test_calculations() -> None:
+@pytest.mark.parametrize(
+    "net_income, total_credits_card_limit, cost_of_living, "
+    "number_of_people_in_household, employment_type, result",
+    [
+        (
+            Money(USD, 1000),
+            Money(USD, 500),
+            Money(USD, 800),
+            3,
+            EmploymentType.CONTRACT_OF_EMPLOYMENT,
+            Money(USD, 22500),
+        )
+    ],
+)
+def test_calculations(
+    net_income,
+    total_credits_card_limit,
+    cost_of_living,
+    number_of_people_in_household,
+    employment_type,
+    result,
+) -> None:
     lending_capacity = calculate_lending_capacity(
-        net_income=Money(USD, 1000),
-        total_credits_card_limit=Money(USD, 500),
-        cost_of_living=Money(USD, 800),
-        number_of_people_in_household=3,
-        employment_type=EmploymentType.CONTRACT_OF_EMPLOYMENT,
+        net_income=net_income,
+        total_credits_card_limit=total_credits_card_limit,
+        cost_of_living=cost_of_living,
+        number_of_people_in_household=number_of_people_in_household,
+        employment_type=employment_type,
     )
 
-    assert lending_capacity == Money(USD, 22500)
+    assert lending_capacity == result
