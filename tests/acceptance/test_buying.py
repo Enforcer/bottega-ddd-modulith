db30afe02b8635df17c043b50ab50cb577d53aae
Sebastian Buczyński <nnplaya@gmail.com>
2023-01-31 22:48:21 +0100

    Implement feature objects

--- tests/acceptance/test_buying.py
+++ tests/acceptance/test_buying.py
@@ -109,9 +109,109 @@ class FeatureObject:
         self._app_client = app_client
 
 
+class Users(FeatureObject):
+    def register(self, username: str, password: str = "foo") -> None:
+        self._app_client.register(username=username, password=password)
+
+    def login(self, username: str, password: str = "foo") -> str:
+        return self._app_client.login(username=username, password=password)
+
+
+class Items(FeatureObject):
+    def add(self, title: str, description: str, price: int, as_user: str) -> None:
+        self._app_client.add_item(
+            title=title, description=description, price=price, user_token=as_user
+        )
+
+
+class Likes(FeatureObject):
+    def like(self, item_id: int, as_user: str) -> None:
+        self._app_client.like(item_id=item_id, user_token=as_user)
+
+    def get_likers(self, item_id: int, as_user: str) -> list[str]:
+        return self._app_client.get_likers(item_id=item_id, user_token=as_user)
+
+
+class Buying(FeatureObject):
+    def buy(self, item_id: int, as_user: str) -> None:
+        self._app_client.buy(item_id=item_id, user_token=as_user)
+
+
+class Payments(FeatureObject):
+    def get_pending(self, as_user: str) -> list[dict]:
+        return self._app_client.get_pending_payments(user_token=as_user)
+
+
 class Catalog(FeatureObject):
-    def search(self, as_user: str) -> list[dict]:
-        pass
+    def search(self, as_user: str, term: str) -> list[dict]:
+        return self._app_client.search(term=term, user_token=as_user)
+
+
+class Negotiations(FeatureObject):
+    def start(
+        self, item_id: int, offer: int, as_user: str, expect_to_fail: bool = False
+    ) -> None:
+        expected_code = 400 if expect_to_fail else 200
+        self._app_client.start_negotiation(
+            item_id=item_id,
+            offer=offer,
+            user_token=as_user,
+            expected_code=expected_code,
+        )
+
+    def list(
+        self, item_id: int, as_user: str, expect_to_fail: bool = False
+    ) -> list[dict]:
+        expected_code = 401 if expect_to_fail else 200
+        return self._app_client.get_negotiations(
+            item_id=item_id, user_token=as_user, expected_code=expected_code
+        )
+
+    def accept(
+        self, item_id: int, offer_id: int, as_user: str, expect_to_fail: bool = False
+    ) -> None:
+        expected_code = 401 if expect_to_fail else 200
+        self._app_client.accept_negotiation(
+            item_id=item_id,
+            offer_id=offer_id,
+            user_token=as_user,
+            expected_code=expected_code,
+        )
+
+
+@pytest.fixture()
+def users(app_client: AppClient) -> Users:
+    return Users(app_client)
+
+
+@pytest.fixture()
+def items(app_client: AppClient) -> Items:
+    return Items(app_client)
+
+
+@pytest.fixture()
+def likes(app_client: AppClient) -> Likes:
+    return Likes(app_client)
+
+
+@pytest.fixture()
+def buying(app_client: AppClient) -> Buying:
+    return Buying(app_client)
+
+
+@pytest.fixture()
+def payments(app_client: AppClient) -> Payments:
+    return Payments(app_client)
+
+
+@pytest.fixture()
+def catalog(app_client: AppClient) -> Catalog:
+    return Catalog(app_client)
+
+
+@pytest.fixture()
+def negotiations(app_client: AppClient) -> Negotiations:
+    return Negotiations(app_client)
 
 
 def delete_user(username: str) -> None:
@@ -124,86 +224,94 @@ def delete_user(username: str) -> None:
     ScopedSession.remove()
 
 
-def test_buying_liked_item(app_client: AppClient) -> None:
+def test_buying_liked_item(
+    users: Users,
+    items: Items,
+    catalog: Catalog,
+    likes: Likes,
+    buying: Buying,
+    payments: Payments,
+) -> None:
     delete_user("seller")
     delete_user("buyer")
     delete_user("second_buyer")
-    app_client.register(username="seller")
-    seller_token = app_client.login(username="seller")
-    app_client.add_item(
+    users.register(username="seller")
+    seller_token = users.login(username="seller")
+    items.add(
         title="Super shoes",
         description="Leather shoes, great for winter",
         price=100,
-        user_token=seller_token,
+        as_user=seller_token,
     )
 
-    app_client.register(username="buyer")
-    buyer_token = app_client.login(username="buyer")
+    users.register(username="buyer")
+    buyer_token = users.login(username="buyer")
 
-    item = app_client.search(term="shoes", user_token=buyer_token)[0]
+    item = catalog.search(term="shoes", as_user=buyer_token)[0]
     assert item["likes"] == 0
 
-    app_client.like(item_id=item["id"], user_token=buyer_token)
+    likes.like(item_id=item["id"], as_user=buyer_token)
 
-    item = app_client.search(term="shoes", user_token=buyer_token)[0]
+    item = catalog.search(term="shoes", as_user=buyer_token)[0]
     assert item["likes"] == 1
 
-    item_likers = app_client.get_likers(item_id=item["id"], user_token=seller_token)
+    item_likers = likes.get_likers(item_id=item["id"], as_user=seller_token)
     assert item_likers == ["buyer"]
 
-    app_client.buy(item_id=item["id"], user_token=buyer_token)
+    buying.buy(item_id=item["id"], as_user=buyer_token)
 
-    app_client.register(username="second_buyer")
-    second_buyer_token = app_client.login(username="second_buyer")
+    users.register(username="second_buyer")
+    second_buyer_token = users.login(username="second_buyer")
 
-    found_items = app_client.search(term="shoes", user_token=second_buyer_token)
+    found_items = catalog.search(term="shoes", as_user=second_buyer_token)
     assert len(found_items) == 0
 
-    pending_payments = app_client.get_pending_payments(user_token=buyer_token)
+    pending_payments = payments.get_pending(as_user=buyer_token)
     assert len(pending_payments) == 1
     assert pending_payments[0]["amount"] == {"amount": 100, "currency": "USD"}
 
 
-def test_buying_negotiated_item(app_client: AppClient) -> None:
+def test_buying_negotiated_item(
+    users: Users,
+    items: Items,
+    catalog: Catalog,
+    likes: Likes,
+    buying: Buying,
+    negotiations: Negotiations,
+    payments: Payments,
+) -> None:
     delete_user("seller")
     delete_user("buyer")
-    app_client.register(username="seller")
-    seller_token = app_client.login(username="seller")
-
-    app_client.add_item(
+    users.register(username="seller")
+    seller_token = users.login(username="seller")
+    items.add(
         title="Super shoes",
         description="Leather shoes, great for winter",
         price=200,
-        user_token=seller_token,
+        as_user=seller_token,
     )
 
-    app_client.register(username="buyer")
-    buyer_token = app_client.login(username="buyer")
+    users.register(username="buyer")
+    buyer_token = users.login(username="buyer")
 
-    item = app_client.search(term="shoes", user_token=buyer_token)[0]
+    item = catalog.search(term="shoes", as_user=buyer_token)[0]
 
-    app_client.start_negotiation(
-        item_id=item["id"], offer=50, user_token=seller_token, expected_code=400
+    negotiations.start(
+        item_id=item["id"], offer=50, as_user=seller_token, expect_to_fail=True
     )
 
-    app_client.start_negotiation(item_id=item["id"], offer=50, user_token=buyer_token)
+    negotiations.start(item_id=item["id"], offer=50, as_user=buyer_token)
 
-    app_client.get_negotiations(
-        item_id=item["id"], user_token=buyer_token, expected_code=401
-    )
+    negotiations.list(item_id=item["id"], as_user=buyer_token, expect_to_fail=True)
 
-    negotiations = app_client.get_negotiations(
-        item_id=item["id"], user_token=seller_token
-    )
-    assert len(negotiations) == 1
-    offer_id = negotiations[0]["id"]
+    negotiations_list = negotiations.list(item_id=item["id"], as_user=seller_token)
+    assert len(negotiations_list) == 1
+    offer_id = negotiations_list[0]["id"]
 
-    app_client.accept_negotiation(
-        item_id=item["id"], offer_id=offer_id, user_token=seller_token
-    )
+    negotiations.accept(item_id=item["id"], offer_id=offer_id, as_user=seller_token)
 
-    app_client.buy(item_id=item["id"], user_token=buyer_token)
+    buying.buy(item_id=item["id"], as_user=buyer_token)
 
-    pending_payments = app_client.get_pending_payments(user_token=buyer_token)
+    pending_payments = payments.get_pending(as_user=buyer_token)
     assert len(pending_payments) == 1
     assert pending_payments[0]["amount"] == {"amount": 50, "currency": "USD"}
