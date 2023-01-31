36a71753b4dc753e43a1f0ba2e6d6b6a1518ef19
Sebastian Buczyński <nnplaya@gmail.com>
2023-01-31 19:30:51 +0100

    Make it working for sqlite

--- used_stuff_market/payments/models.py
+++ used_stuff_market/payments/models.py
@@ -1,15 +1,18 @@
 from sqlalchemy import Column, DateTime, Numeric, String
 from sqlalchemy.dialects.postgresql import UUID
 
-from used_stuff_market.db import Base
+from used_stuff_market.db import Base, guid
 
 
 class Payment(Base):
     __tablename__ = "payments"
-    __table_args__ = {"schema": "payments"}
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True)
-    owner_id = Column(UUID(as_uuid=True), nullable=False)
+    uuid = Column(
+        UUID(as_uuid=True).with_variant(guid.GUID, "sqlite"), primary_key=True
+    )
+    owner_id = Column(
+        UUID(as_uuid=True).with_variant(guid.GUID, "sqlite"), nullable=False
+    )
     created_at = Column(DateTime(), nullable=False)
     amount = Column(Numeric(), nullable=False)
     currency = Column(String(3), nullable=False)
