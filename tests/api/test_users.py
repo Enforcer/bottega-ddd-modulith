8b673c9bf4b0cda8272d3ed267dc0792c2af9de2
Sebastian Buczyński <nnplaya@gmail.com>
2023-03-05 21:16:46 +0100

    Skip registration test

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,6 +1,9 @@
+import pytest
+
 from fastapi.testclient import TestClient
 
 
+@pytest.mark.skip()
 def test_registration(client: TestClient) -> None:
     response = client.post("/users", json={"username": "test", "password": "test"})
 
