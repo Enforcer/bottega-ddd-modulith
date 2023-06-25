dea43b577a683c49180319c16da54ab51b2f5f2d
Sebastian Buczyński <nnplaya@gmail.com>
2023-06-25 17:32:08 +0200

    Implement Negotiation aggregate

--- tests/negotiations/test_negotiation.py
+++ tests/negotiations/test_negotiation.py
@@ -1,1 +1,72 @@
-from used_stuff_market.negotiations.negotiation import Negotiation
+from decimal import Decimal
+
+import factory
+import pytest
+
+from used_stuff_market.negotiations.negotiation import Negotiation, State
+
+ITEM_ID = 1
+SELLER_ID = 2
+BUYER_ID = 3
+OTHER_DUDE = 4
+
+
+class NegotiationFactory(factory.Factory):
+    class Meta:
+        model = Negotiation
+
+    item_id = ITEM_ID
+    seller_id = SELLER_ID
+    buyer_id = BUYER_ID
+    state = State.WAITING_FOR_SELLER
+    price = Decimal("9.99")
+    currency = "USD"
+
+
+def test_accepted_negotiation_cannot_be_accepted_again() -> None:
+    negotiation = NegotiationFactory.build()
+    negotiation.accept(user_id=SELLER_ID)
+
+    with pytest.raises(Negotiation.NegotiationConcluded):
+        negotiation.accept(user_id=SELLER_ID)
+
+
+def test_cannot_be_accepted_by_seller_if_they_offered_price() -> None:
+    negotiation = NegotiationFactory.build(state=State.WAITING_FOR_BUYER)
+
+    with pytest.raises(Negotiation.WaitingForOtherParticipant):
+        negotiation.accept(user_id=SELLER_ID)
+
+
+def test_accepted_negotiation_cannot_be_broken_off() -> None:
+    negotiation = NegotiationFactory.build()
+    negotiation.accept(user_id=SELLER_ID)
+
+    with pytest.raises(Negotiation.NegotiationConcluded):
+        negotiation.break_off(user_id=SELLER_ID)
+
+
+def test_cannot_be_accepted_by_someone_other_than_buyer_or_seller() -> None:
+    negotiation = NegotiationFactory.build()
+    with pytest.raises(Negotiation.NotAParticipant):
+        negotiation.accept(user_id=OTHER_DUDE)
+
+
+def test_cannot_be_broken_off_by_someone_other_than_buyer_or_seller() -> None:
+    negotiation = NegotiationFactory.build()
+    with pytest.raises(Negotiation.NotAParticipant):
+        negotiation.break_off(user_id=OTHER_DUDE)
+
+
+def test_cannot_be_counter_offered_by_someone_other_than_buyer_or_seller() -> None:
+    negotiation = NegotiationFactory.build()
+    with pytest.raises(Negotiation.NotAParticipant):
+        negotiation.counteroffer(
+            user_id=OTHER_DUDE, price=Decimal("12.99"), currency="USD"
+        )
+
+
+def test_counter_offering_changes_price() -> None:
+    negotiation = NegotiationFactory.build(price=Decimal("9.99"))
+    negotiation.counteroffer(user_id=SELLER_ID, price=Decimal("12.99"), currency="USD")
+    assert negotiation.price == Decimal("12.99")
--- used_stuff_market/negotiations/negotiation.py
+++ used_stuff_market/negotiations/negotiation.py
@@ -1,17 +1,75 @@
 from decimal import Decimal
+from enum import StrEnum
+
+from attr import define
 
 
+class State(StrEnum):
+    WAITING_FOR_SELLER = "waiting_for_seller"
+    WAITING_FOR_BUYER = "waiting_for_buyer"
+    BROKEN_OFF = "broken_off"
+    ACCEPTED = "accepted"
+
+    @property
+    def concluded(self) -> bool:
+        return self in (State.BROKEN_OFF, State.ACCEPTED)
+
+
+@define
 class Negotiation:
-    def __init__(self, item_id: int, seller_id: int, buyer_id: int) -> None:
-        self._item_id = item_id
-        self._seller_id = seller_id
-        self._buyer_id = buyer_id
+    _item_id: int
+    _seller_id: int
+    _buyer_id: int
+    _state: State
+    _price: Decimal
+    _currency: str
+
+    class NegotiationConcluded(Exception):
+        pass
+
+    class NotAParticipant(Exception):
+        pass
+
+    class WaitingForOtherParticipant(Exception):
+        pass
+
+    @property
+    def price(self) -> Decimal:
+        return self._price
 
     def accept(self, user_id: int) -> None:
-        pass
+        self._validate_participant(user_id)
+        self._validate_side(user_id)
+        self._validate_not_concluded()
+
+        self._state = State.ACCEPTED
 
     def break_off(self, user_id: int) -> None:
-        pass
+        self._validate_participant(user_id)
+        self._validate_not_concluded()
+
+        self._state = State.BROKEN_OFF
 
     def counteroffer(self, user_id: int, price: Decimal, currency: str) -> None:
-        pass
+        self._validate_participant(user_id)
+
+        self._price = price
+        self._currency = currency
+        if user_id == self._seller_id:
+            self._state = State.WAITING_FOR_BUYER
+        else:
+            self._state = State.WAITING_FOR_SELLER
+
+    def _validate_participant(self, user_id: int) -> None:
+        if user_id not in (self._seller_id, self._buyer_id):
+            raise self.NotAParticipant
+
+    def _validate_not_concluded(self) -> None:
+        if self._state.concluded:
+            raise self.NegotiationConcluded
+
+    def _validate_side(self, user_id: int) -> None:
+        if self._state == State.WAITING_FOR_SELLER and user_id != self._seller_id:
+            raise self.WaitingForOtherParticipant
+        elif self._state == State.WAITING_FOR_BUYER and user_id != self._buyer_id:
+            raise self.WaitingForOtherParticipant
