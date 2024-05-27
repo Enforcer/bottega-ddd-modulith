924705352e6bd74d738a66059fad3d9a1a0ea0bc
Sebastian Buczyński <nnplaya@gmail.com>
2024-05-27 07:50:11 +0200

    Generate empty Django app

---
+++ with_django/README.md
@@ -0,0 +1,8 @@
+# Clean slate - with Django
+
+## Creating new application (Django component)
+
+```bash
+cd used_stuff_market
+./manage.py startapp used_stuff_market
+```
---
+++ with_django/used_stuff_market/used_stuff_market/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for used_stuff_market project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "used_stuff_market.settings")
+
+application = get_asgi_application()
