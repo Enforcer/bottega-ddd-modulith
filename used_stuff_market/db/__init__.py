801039343ef0d6b516fdede41da5677e2458cd27
Sebastian Buczyński <nnplaya@gmail.com>
2025-09-14 19:23:15 +0200

    Move session lifecycle context manager to container

--- used_stuff_market/db/__init__.py
+++ used_stuff_market/db/__init__.py
@@ -1,9 +1,5 @@
-from contextlib import contextmanager
-from typing import Iterator
-
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import (
-    Session as SessionCls,
     DeclarativeBase,
     registry,
     sessionmaker,
@@ -23,14 +19,3 @@ class Base(DeclarativeBase):
 
 
 mapper_registry = registry(metadata=metadata)
-
-
-@contextmanager
-def db_session() -> Iterator[SessionCls]:
-    session = session_factory()
-    try:
-        yield session
-    except Exception:
-        raise
-    finally:
-        session.close()
--- used_stuff_market/main/container.py
+++ used_stuff_market/main/container.py
@@ -1,18 +1,46 @@
-from typing import Iterator
+from contextlib import contextmanager
+from typing import ContextManager, Iterator
 
-from lagom import Container, context_dependency_definition, ContextContainer
+from lagom import (
+    Container,
+    context_dependency_definition,
+    ContextContainer,
+    UnresolvableTypeDefinition,
+)
 from lagom.integrations.fast_api import FastApiIntegration
 from sqlalchemy.orm import Session
 
-from used_stuff_market.db import db_session
+from used_stuff_market.db import session_factory
 
 container = Container()
+container[Session] = UnresolvableTypeDefinition(
+    "Use ContextManager[Session] to get Session instance."
+)
+
 deps = FastApiIntegration(container, request_context_singletons=[Session])
 # Documentation: https://lagom-di.readthedocs.io/en/latest/framework_integrations/#fastapi
 context_container = ContextContainer(container, context_types=[Session])
 
 
 @context_dependency_definition(container)  # type: ignore
-def request_scoped_session() -> Iterator[Session]:
-    with db_session() as session:
+def session_with_cleanup() -> Iterator[Session]:
+    session = session_factory()
+    try:
         yield session
+    finally:
+        session.close()
+
+
+@contextmanager
+def manage_session(a_container: Container) -> Iterator[Container]:
+    """This context manager ensures there is a Session instance in the container.
+
+    First, it ensures that always the same session will be returned (.temporary_singletons).
+    Then, it initiates session using ContextManager[Session].
+    Finally, a container is cloned to be reconfigured for returning the constructed Session.
+    """
+    with a_container.temporary_singletons(singletons=[Session]):
+        with a_container[ContextManager[Session]] as _session:  # type: ignore
+            container_with_session = a_container.clone()
+            container_with_session[Session] = _session
+            yield container_with_session
